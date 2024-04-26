f73b22a3105d79cbde55c1e84e6c5707babd196b
4akislav <andron2588@gmail.com>
2024-04-26 19:17:15 +0300

    struct move to folder, city logic, celcium temp in str

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"goBot/structs"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -16,39 +17,12 @@ import (
 	"github.com/joho/godotenv"
 )
 
-const prefix string = "!bot"
-
-type Weather struct {
-	Location struct {
-		Name    string `json:"name"`
-		Country string `json:"country"`
-	} `json:"location"`
-	Current struct {
-		Temp float32 `json:"temp_c"`
-	} `json:"current"`
-}
+const prefix string = "!go"
 
 func main() {
 	godotenv.Load()
 
-	weather_url := "https://api.weatherapi.com/v1/current.json?key=" + os.Getenv("WEATHER_TOKEN") + "&q=Kyiv&aqi=no&lang=ja"
-
-	response, err := http.Get(weather_url)
-	if err != nil {
-		log.Fatal("error making HTTP request", err)
-	}
-	defer response.Body.Close()
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal("error reading response body", err)
-	}
-
-	var weatherData Weather
-	err = json.Unmarshal(body, &weatherData)
-	if err != nil {
-		log.Fatal("error parsing JSON", err)
-	}
+	var locationName string
 
 	token := os.Getenv("BOT_TOKEN")
 
@@ -68,6 +42,11 @@ func main() {
 			return
 		}
 
+		if len(args) == 1 {
+			message := fmt.Sprintln("?")
+			s.ChannelMessageSend(m.ChannelID, message)
+			return
+		}
 		if args[1] == "hello" {
 			s.ChannelMessageSend(m.ChannelID, "world!")
 		}
@@ -86,8 +65,26 @@ func main() {
 			s.ChannelMessageSend(m.ChannelID, nums[selection])
 		}
 
-		if args[1] == "weather" {
-			message := fmt.Sprintf("Місто %s", weatherData.Location.Name)
+		if len(args) > 2 && args[1] == "weather" {
+			locationName = args[2]
+
+			weatherData, err := getWeather(locationName)
+			if err != nil {
+				s.ChannelMessageSend(m.ChannelID, "Сталась помилка, можливо ви не привильно ввели назву міста")
+				return
+			}
+
+			message := fmt.Sprintf("Місто %s, країна %s, %f°C", weatherData.Location.Name, weatherData.Location.Country, weatherData.Current.Temp)
+			s.ChannelMessageSend(m.ChannelID, message)
+
+		}
+
+		if args[1] == "weather" && len(args) == 2 {
+			message := "Бачу ти хочеш дізнатись погоду в якомусь місті, напиши англійською назву міста. Приклад - !go weather Kyiv"
+			s.ChannelMessageSend(m.ChannelID, message)
+		}
+		if args[1] != structs.CommandsList.Dice && args[1] != structs.CommandsList.ShowWeather && args[1] != structs.CommandsList.TestHello {
+			message := "Вибач!\nАле я не розумію що ти від мене хочеш, я знаю тільки певний перелік команд!\nТа з радістю їх виконаю!\nТа пам'ятай що я маю своє їм'я, щоб мене покликати пиши просто !go \"команда\""
 			s.ChannelMessageSend(m.ChannelID, message)
 		}
 	})
@@ -105,4 +102,26 @@ func main() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
+}
+
+func getWeather(locationName string) (*structs.Weather, error) {
+	weather_url := "https://api.weatherapi.com/v1/current.json?key=" + os.Getenv("WEATHER_TOKEN") + "&lang=uk&q=" + locationName + "&aqi=no"
+
+	response, err := http.Get(weather_url)
+	if err != nil {
+		log.Fatal("error making HTTP request", err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal("error reading response body", err)
+	}
+
+	var weatherData structs.Weather
+	err = json.Unmarshal(body, &weatherData)
+	if err != nil {
+		log.Fatal("error parsing JSON", err)
+	}
+	return &weatherData, err
 }
